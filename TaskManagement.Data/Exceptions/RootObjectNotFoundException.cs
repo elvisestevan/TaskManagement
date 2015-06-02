2fc9ec099285b9e217468f25a5ebab21cd20e344
Elvis Estevan <eestevan@cntlog.com.br>
2015-06-01 23:27:26 -0300

    Error Handling configurado

---
+++ TaskManagement.Data/Exceptions/RootObjectNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskManagement.Data.Exceptions
+{
+    public class RootObjectNotFoundException : Exception
+    {
+
+        public RootObjectNotFoundException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
--- TaskManagement.Web.Api/App_Start/WebApiConfig.cs
+++ TaskManagement.Web.Api/App_Start/WebApiConfig.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
+using System.Web.Http.ExceptionHandling;
 using System.Web.Http.Routing;
 using System.Web.Http.Tracing;
 using TaskManagement.Common.Logging;
 using TaskManagement.Web.Common;
+using TaskManagement.Web.Common.ErrorHandling;
 using TaskManagement.Web.Common.Routing;
 
 namespace TaskManagement.Web.Api
@@ -23,6 +25,8 @@ namespace TaskManagement.Web.Api
 
             config.Services.Replace(typeof(IHttpControllerSelector), new NamespaceHttpControllerSelector(config));
             config.Services.Replace(typeof(ITraceWriter), new SimpleTraceWriter(WebContainerManager.Get<ILogManager>()));
+            config.Services.Add(typeof(IExceptionLogger), new SimpleExceptionLogger(WebContainerManager.Get<ILogManager>()));
+            config.Services.Replace(typeof(IExceptionHandler), new GlobalExceptionHandler());
         }
     }
 }
